ff4fdec0f3b2eda643fc654d5e1b746f3822d42f
Samuel Guedes <guedes.works7@gmail.com>
2022-02-28 21:59:57 -0400

    feat: move file location

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -1,5 +1,5 @@
 defmodule Rockelivery.Users.Create do
-  alias Rockelivery.{Repo, User}
+  alias Rockelivery.{Error, Repo, User}
 
   def call(params) do
     params
@@ -9,8 +9,5 @@ defmodule Rockelivery.Users.Create do
   end
 
   defp handle_insert({:ok, %User{}} = result), do: result
-
-  defp handle_insert({:error, result}) do
-    {:error, %{status: :bad_request, result: result}}
-  end
+  defp handle_insert({:error, result}), do: {:error, Error.build(:bad_request, result)}
 end
